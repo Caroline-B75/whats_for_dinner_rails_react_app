cb4e55590ccb3b13cd0e08a26ca5d747a3d8c3f6
Caroline Belmas <caroline.belmas@gmail.com>
2021-05-03 18:27:03 +0200

    create accesses table

--- db/migrate/20210503162159_create_accesses.rb
+++ db/migrate/20210503162159_create_accesses.rb
@@ -1,10 +1,8 @@
 class CreateAccesses < ActiveRecord::Migration[6.0]
   def change
     create_table :accesses do |t|
-      t.references :menu, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-
-      t.timestamps
+      t.references :menu, null: false, foreign_key: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_161427) do
+ActiveRecord::Schema.define(version: 2021_05_03_162159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accesses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "menu_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["menu_id"], name: "index_accesses_on_menu_id"
+    t.index ["user_id"], name: "index_accesses_on_user_id"
+  end
 
   create_table "favorite_recipes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -87,6 +96,8 @@ ActiveRecord::Schema.define(version: 2021_05_03_161427) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accesses", "menus"
+  add_foreign_key "accesses", "users"
   add_foreign_key "favorite_recipes", "recipes"
   add_foreign_key "favorite_recipes", "users"
   add_foreign_key "menus", "users"
