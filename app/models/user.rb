d1ff8248b9fa9e1e1122e20bb57ffe7fd8fc142f
Caroline Belmas <caroline.belmas@gmail.com>
2021-05-19 18:16:41 +0200

    Adding has many menus, reviews, favorite_recipes to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :menus
+  has_many :reviews
+  has_many :favorite_recipes
+  has_many :favorites, through: :favorite_recipes, source: :recipe
   
   validates :username, presence: true
   validates :first_name, presence: true
